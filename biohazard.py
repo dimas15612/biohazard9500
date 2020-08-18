e14bac4b6436a39fa06c4f03bec28f35de98d27f
biogeorgelab <69615537+biogeorgelab@users.noreply.github.com>
2020-08-18 07:59:34 +0300

    Update biohazard.py

--- biohazard.py
+++ biohazard.py
@@ -8,7 +8,7 @@ class MyClient(discord.Client):
 	msg2 = 0
 	async def on_ready(self):
 		print('Вошёл как', self.user)
-		await client.change_presence(activity = discord.Game('Крутой бот | v2.2')
+		await client.change_presence(activity = discord.Game('Крутой бот | v2.2'))
 	async def on_message(self, message):
 		c = 0
 		r = 0
