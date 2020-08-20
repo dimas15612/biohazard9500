635b428bd22b5405d45ab6110ba4d30eb9b163a3
biogeorgelab <69615537+biogeorgelab@users.noreply.github.com>
2020-08-20 09:13:50 +0300

    Update biohazard.py

--- biohazard.py
+++ biohazard.py
@@ -69,6 +69,13 @@ class MyClient(discord.Client):
 			embed.add_field(name='*випроли+', value='Создать роли со своим сообщением', inline=False)
 			embed.add_field(name='*випфлуд', value='Флуд своим сообщением', inline=False)
 			await message.author.send(embed=embed)
+		#if message.author.id == 484230158731968521 or message.author.id == 351686223196192768:
+			#if message.content.startswith('*bl'):
+				#await message.delete()
+				#mc = message.content.replace('*bl', '')
+				#f = open('blacklist.txt', 'w')
+				#f.write(str(open('blacklist.txt', 'r')) + str(mc) + '\n')
+				#f.close()
 		if white.find(str(message.guild.id)) == -1 and black.find(str(message.author.id)) == -1:
 			if message.content == '*атака': #автоматический краш
 				await message.delete() #удаление сообщения
